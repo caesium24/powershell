7e18c1cca250e044006a493118ee2407e6804e97
Xerathe29 <42093909+Xerathe29@users.noreply.github.com>
2020-06-15 13:51:05 -0500

    Update Receive-DisaCertificate.ps1
    Added help comment block.
    Modified FilePath parameter on line 52 to name each raw text file properly in the $SavePath location.

--- Certificate_Automation/Receive-DisaCertificate.ps1
+++ Certificate_Automation/Receive-DisaCertificate.ps1
@@ -1,3 +1,28 @@
+<#
+ .NOTES
+    Version  : 1.0
+    Author   : Eshton Brogan & Sid Johnston
+    Created  : 15 June 2020
+ 
+ .SYNOPSIS
+  Pulls raw HTML for completed certificate requests from a specified Certificate Authority website.
+
+ .DESCRIPTION
+  Utilizing user-provided information and a Certificate Request CSV, this function outputs raw text files which contain
+  certificate hash values. 
+  
+ .PARAMETER CSVPath
+  File path of CSV which contains the status URL(s) for submitted certificate request(s).
+
+ .PARAMETER CA
+  Base URL for the DISA Certificate Authority website.
+  
+ .PARAMETER SavePath
+  File path where raw HTML files will be saved. 
+
+ .EXAMPLE
+  Receive-DisaCertificate -CSVPath C:\temp\certificate_request.csv -CA "http://ca-1.csd.disa.mil" -SavePath C:\temp\full 
+#>
 function Receive-DisaCertificate {
     [CmdletBinding()]
     param (
@@ -24,6 +49,6 @@ function Receive-DisaCertificate {
         $webreq = Invoke-WebRequest -Uri "$($CA)/ca/ee/ca/checkRequest?requestId=$($reqID)"
         $link = $webreq.links.href[1]
         $webreq = Invoke-WebRequest -Uri "$($CA)/ca/ee/ca/$($link)"
-        $webreq.ParsedHtml.IHTMLDocument3_documentelement.outerText | Out-file -FilePath $SavePath
+        $webreq.ParsedHtml.IHTMLDocument3_documentelement.outerText | Out-file -FilePath "$SavePath\$($name).txt"
     }
 }
